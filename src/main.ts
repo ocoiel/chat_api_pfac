e5b93ed44dd0040bbe18e31371e40a50a320dc91
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-11-30 00:24:57 -0300

    prettier: single quote

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from "@nestjs/platform-fastify";
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,14 +13,14 @@ async function bootstrap() {
   );
 
   const config = new DocumentBuilder()
-    .setTitle("API Chat - PFAC")
-    .setDescription("Desafio Play For A Cause - NestJS")
-    .setVersion("1.0")
-    .addTag("chat")
+    .setTitle('API Chat - PFAC')
+    .setDescription('Desafio Play For A Cause - NestJS')
+    .setVersion('1.0')
+    .addTag('chat')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3333);
 }
--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,12 +1,12 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   // Log of queries in database
   constructor() {
     super({
-      log: ["query"],
+      log: ['query'],
     });
   }
   async onModuleInit() {
