28c51be01e1a1197f43b2c7b4a62dd054fb0c811
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-11-30 00:54:30 -0300

    feat: create-user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { User } from '../entities/user.entity';
+import { IsEmail, IsString } from 'class-validator';
+
+export class CreateUserDto extends User {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  name: string;
+}
