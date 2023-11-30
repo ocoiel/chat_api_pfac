d7f95491ddd07e99d878dfb53db7218b0cdaadce
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-11-30 01:13:31 -0300

    chore(auth): change from `name` to `username` in model

--- src/auth/models/UserFromJwt.ts
+++ src/auth/models/UserFromJwt.ts
@@ -1,5 +1,5 @@
 export interface UserFromJwt {
   id: number;
   email: string;
-  name: string;
+  username: string;
 }
--- src/auth/models/UserPayload.ts
+++ src/auth/models/UserPayload.ts
@@ -1,7 +1,7 @@
 export interface UserPayload {
   sub: number;
   email: string;
-  name: string;
+  username: string;
   iat?: number;
   exp?: number;
 }
