3283097183a9fd97689f810383c4324caa9175e0
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-11-30 00:15:46 -0300

    feat: setup swagger on project

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+} from "@nestjs/platform-fastify";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle("API Chat - PFAC")
+    .setDescription("Desafio Play For A Cause - NestJS")
+    .setVersion("1.0")
+    .addTag("chat")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3333);
 }
 bootstrap();
