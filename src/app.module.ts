db68925aada11e1a0b9bce4ae231752e1e91f53d
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-11-30 00:49:59 -0300

    feat: create a module and service for prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, PrismaModule],
   controllers: [],
   providers: [],
 })
