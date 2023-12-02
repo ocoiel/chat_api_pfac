fab26f0cbee138bf1b217b775345943327c2da08
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-12-02 15:37:12 -0300

    feat: add seed for prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      id: 1,
+      email: 'pfca@pfca.com',
+      username: 'pfca',
+      password: 'pfca',
+    },
+  });
+
+  console.log('Seed data inserted successfully');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
